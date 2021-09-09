82f6f31ca95732f2ef836e56d03d3b1664da12b2
Mohammad-Saqib1 <87147700+Mohammad-Saqib1@users.noreply.github.com>
2021-09-10 00:05:45 +0530

    Add files via upload

---
+++ alldirmoving.py
@@ -0,0 +1,49 @@
+import pygame
+import time
+pygame.init()
+#colors
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+
+
+#screen l and b
+scrn_width=900
+scrn_height=600
+#create window
+gamewindow=pygame.display.set_mode((scrn_width,scrn_height))
+pygame.display.set_caption("SnakesBySaqib")
+pygame.display.update()
+#variables
+exit_game=False
+game_over=False
+snake_x=45
+snake_y=55
+snake_size=10
+clock=pygame.time.Clock()
+fps=30
+#game loop
+
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit_game=True
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                snake_x=snake_x+10
+            if event.key==pygame.K_LEFT:
+                snake_x=snake_x-10
+            if event.key==pygame.K_UP:
+                snake_y=snake_y-10
+            if event.key==pygame.K_DOWN:
+                snake_y=snake_y+10
+
+    gamewindow.fill(white)
+    #create head of snake
+
+    pygame.draw.rect(gamewindow,black,[snake_x,snake_y,snake_size,snake_size])
+    clock.tick(fps)
+    pygame.display.update()
+
+pygame.quit()
+quit()
