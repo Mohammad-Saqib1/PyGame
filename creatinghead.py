deba0866cb578d97094b25a6a1f2f960412856f3
Mohammad-Saqib1 <87147700+Mohammad-Saqib1@users.noreply.github.com>
2021-09-10 00:04:08 +0530

    Add files via upload

---
+++ creatinghead.py
@@ -0,0 +1,36 @@
+import pygame
+pygame.init()
+#colors
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+
+
+#screen l and b
+scrn_width=900
+scrn_height=600
+#create window
+gamewindow=pygame.display.set_mode((scrn_width,scrn_height))
+pygame.display.set_caption("SnakesBySaqib")
+pygame.display.update()
+#variables
+exit_game=False
+game_over=False
+snake_x=45
+snake_y=55
+snake_size=10
+
+#game loop
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit_game=True
+
+    gamewindow.fill(white)
+    #create head of snake
+    pygame.draw.rect(gamewindow,black,[snake_x,snake_y,snake_size,snake_size])
+
+    pygame.display.update()
+
+pygame.quit()
+quit()
